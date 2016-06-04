747a23830a93d95d78e9644ffe843b2819f8de06
Holly French <hollyafrench90@gmail.com>
2016-06-04 01:01:14 -0700

    Update requirements

--- tests/test_location_utils.py
+++ tests/test_location_utils.py
@@ -8,3 +8,9 @@ def test_get_region():
     assert get_region(40.053116, -76.313603, "country") == "US"
     assert get_region(40.053116, -76.313603, "state") == "PA"
     assert get_region(40.053116, -76.313603, "city") == "Lancaster"
+
+
+def test_get_place():
+    assert get_place("8km ENE of Eielson Air Force Base, Alaska") == "Eielson Air Force Base, Alaska"
+    assert get_place("Next Door") == "Next Door"
+    assert get_place("") == ""
